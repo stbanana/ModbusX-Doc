bdb09a2f5c719baa4c1c3139f175530e1d4d9431
yono <yono233@outlook.com>
2024-11-13 11:17:41 +0800

    Updates

--- scripts/deploy-gh-pages.js
+++ scripts/deploy-gh-pages.js
@@ -5,11 +5,5 @@ var ghpages = require('gh-pages');
 main();
 
 function main() {
-    ghpages.publish('./gh-pages', (err) => {
-        if (err) {
-            console.error('Deployment failed:', err);
-        } else {
-            console.log('Deployment successful!');
-        }
-    });
+    ghpages.publish('./gh-pages', console.error.bind(console));
 }
